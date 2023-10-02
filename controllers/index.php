9267e5e785cb07a04b8db54eff929d01620f17a9
Wassila <wassilaboukedroun@outlook.fr>
2023-10-02 16:48:56 +0200

    Api_back dernier jour Abraham

---
+++ controllers/index.php
@@ -0,0 +1,43 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+// inclure functions.php
+require_once "userController.php";
+
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    $url = $_SERVER["REQUEST_URI"];
+    $url = trim($url, "\/");
+    $url = explode("/", $url);
+    $action = $url[1];
+    if($action == "getuserlist"){
+       UserController::loadModel($action);
+    }
+    else if($action == "getListMessage"){
+       UserController::loadModel($action,[$url[2], $url[3]]);
+    }else{
+        echo json_encode([
+            "status" => 404,
+            "message" => "not found"
+        ]);
+    }
+}else{
+    // ce que l'utilisateur envoi via un formulaire on recupere
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if($data["action"] == "login"){
+        // appel de la fonction login
+        UserController::loadModel($data['pseudo'], $data['password']);
+    }else if($data["action"] == "register"){
+        // on fais appel a la fonction register pour enregistre le user
+        UserController::loadModel($data["action"], [$data['firstname'], $data['lastname'], $data['pseudo'], $data['password']]);
+    }else if($data["action"] == "send message"){
+        // appel de la fonction sendMessage
+        UserController::loadModel($data["action"], [$data['expeditor'],$data['receiver'], $data['message']]);
+    }else{
+        echo json_encode([
+                    "status" => 404,
+                    "message" => "service not found",
+                ]);
+    }
+
+}
---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+// fonction pour ce connecter a la bd
+
+// function pour enregistrer un utilisateur
+
+
+
+
+
+// fonction pour recuperer la liste des users
+function getListUser(){
+    // conexion a la bd
+    $db = dbConnect();
+
+}
+
